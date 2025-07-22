ecedb46e14ee294446f4428fca1060f5dfa858b1
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2025-07-21 23:26:37 -0300

    add prettier and eslint

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,9 @@
+#### JIRA card
+
+venice-music.atlassian.net/browse/
+
+#### Description
+
+[//]: # "A few sentences describing the overall goals of the pull request's commits"
+
+#### Screenshots
--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+import prettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
     ],
+    rules: {
+      'prettier/prettier': 'warn',
+    },
     languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
